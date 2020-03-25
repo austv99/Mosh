c24acbb985463b1e5509b087f1c50307832303fa
AlvinRC <alvinrcheng@outlook.com>
2020-03-25 15:22:55 +1100

    added basic youtube backend

--- SourceCode_and_Documentation/mosh/src/App.js
+++ SourceCode_and_Documentation/mosh/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
+//import { Router, Link } from "@reach/router";
 import './App.css';
+import YoutubeResult from './YoutubeResult';
 
 function App() {
   return (
@@ -18,6 +20,7 @@ function App() {
         >
           Learn React
         </a>
+        <YoutubeResult />
       </header>
     </div>
   );
---
+++ SourceCode_and_Documentation/mosh/src/YoutubeResult.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from 'react';
+
+const YoutubeResult= () => {
+
+    const [search,setSearch] = useState("");
+    //var playlistID = "PLzMXToX8Kzqggrhr-v0aWQA2g8pzWLBrR";
+    var userKey = 'AIzaSyBhklDEhDYrLwf5mMkLKsA34Btqjpj8S7k';
+
+    //working playlist url
+    //const URL = ` https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&key=${userKey}&playlistId=${playlistID}`;
+    //note cannot add new line within url
+    var InputTitle="DDU DDU"
+    const options = {
+        part:'snippet',
+        key:userKey,
+        regionCode:'AU',
+        q:`${InputTitle} Official Music Video`,
+        maxResults:10,
+        $videoCategoryId:10,
+    }
+    const query = Object.keys(options).map(k => encodeURIComponent(k) + '=' + encodeURIComponent(options[k])).join('&');
+    //?part=snippet&key=${userKey}&regionCode=AU&q=music&maxResults='10'$videoCategoryId=10
+    //working search url
+    const URL = ' https://www.googleapis.com/youtube/v3/search?' + query;
+    useEffect(() => {
+        // GET request using fetch inside useEffect React hook
+        fetch(URL)
+            .then(response => response.json())
+            .then(responseData => {
+                var title = (responseData.items).map(a => a.snippet.title);
+                console.log(title);
+                //title is an array
+                
+                setSearch(title[0])
+            })
+            ;
+    
+    // empty dependency array means this effect will only run once (like componentDidMount in classes)
+    }, [URL]);
+    return (
+        <div className="search-params">
+            {search}
+        </div>
+    );
+};
+export default YoutubeResult;
