561159ea2edf4774bbb6c17694fed192efedcb7f
Alvin <alvinrcheng@gmail.com>
2020-04-22 13:11:09 +1000

    discover music and share bugged

--- SourceCode_and_Documentation/mosh/src/components/navBar.jsx
+++ SourceCode_and_Documentation/mosh/src/components/navBar.jsx
@@ -12,7 +12,7 @@ import PersonIcon from '@material-ui/icons/Person';
 import UserProfileModal from "./userProfileModal"
 import { makeStyles } from '@material-ui/core/styles';
 import { fire } from '../config/fire'
-
+/* global gapi */
 
  let avatar_url = "https://scontent.fsyd4-1.fna.fbcdn.net/v/t1.0-9/p960x960/83084529_2426902500959534_3911554766622162944_o.jpg?_nc_cat=111&_nc_sid=09cbfe&_nc_oc=AQllXWmPqGoO6LfqqdQXj3li1iED8jgdjzvkZpYuheTtHyu36Z6s40d69qRY94r_bx4&_nc_ht=scontent.fsyd4-1.fna&_nc_tp=6&oh=37b6656b3e75b655ea06f9a44eb9c2e9&oe=5EBAD30E";
 
@@ -38,6 +38,7 @@ function NavBar(props) {
     };
 
     const handleSignOut = () => {
+        console.log(gapi)
         fire.auth().signOut().then(() => {
             console.log("user signed out")
         }).catch(err => {
@@ -58,7 +59,7 @@ function NavBar(props) {
                 <MenuIcon />
             </IconButton>
             <Typography variant="h6" style = {{ flexGrow : 1 }}>
-                <Link to="/home" style={{ textDecoration: 'none', color: 'inherit '}}>
+                <Link to={"/home/token/" + props.token} style={{ textDecoration: 'none', color: 'inherit '}}>
                     Mosh
                 </Link>
             </Typography>
@@ -102,12 +103,12 @@ function NavBar(props) {
                         <ExitToAppIcon />
                     </Link>
                 </div> */}
-                <Link to="/discover" style={{ textDecoration: 'none', color: 'inherit ', margin: "5px"}}>
+                <Link to={"/discover/token/" + props.token} style={{ textDecoration: 'none', color: 'inherit ', margin: "5px"}}>
                     <IconButton className = {classes.iconButton}>
                         <SearchIcon />
                     </IconButton>
                 </Link>
-                <Link to="/share" style={{ textDecoration: 'none', color: 'inherit', margin: "5px"}}>
+                <Link to={"/share/token/" + props.token} style={{ textDecoration: 'none', color: 'inherit', margin: "5px"}}>
                     <IconButton className = {classes.iconButton}>
                         <ShareIcon />
                     </IconButton>
